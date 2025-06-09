d3105fa8fb37eb25c59aaeaccb61d4e2e028ae57
maamokun/MikanDev <sachadeco214@gmail.com>
2025-06-09 16:30:15 +0900

    add: migrations

---
+++ prisma/migrations/20250609064649_stream_persistence/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Stream" (
+    "id" TEXT NOT NULL,
+    "streamId" TEXT NOT NULL,
+
+    CONSTRAINT "Stream_pkey" PRIMARY KEY ("id")
+);
---
+++ prisma/migrations/20250609072730_stream_ids/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Stream" ADD COLUMN     "chatId" TEXT NOT NULL DEFAULT 'null';
